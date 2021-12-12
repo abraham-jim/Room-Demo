170f48af81c45a05c58a98d3d930ec50466e84e5
abraham-jim <abrahamjima@gmail.com>
2021-12-11 20:50:40 -0800

    Save all files

---
+++ app/src/main/java/com/example/roomdemo/EmployeeApp.kt
@@ -0,0 +1,12 @@
+package com.example.roomdemo
+
+import android.app.Application
+//Todo 7: create the application class and initialize the database
+class EmployeeApp: Application(){
+
+    val db by lazy { EmployeeDatabase.getInstance(this)
+    }
+
+}
+
+//    val employeeDao = (application as EmployeeApp).db.employeeDao()
